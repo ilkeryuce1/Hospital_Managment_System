cffa753de5ec9cc93db280697363e779d8706e86
ilkeryuce1 <109898238+ilkeryuce1@users.noreply.github.com>
2022-12-30 20:42:45 +0300

    Online Hospital

---
+++ MVC_Hospital/Controllers/LabAppointmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Hospital.Controllers
+{
+    public class LabAppointmentController : Controller
+    {
+        // GET: LabAppointment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_Hospital/Controllers/PatientAppointController.cs
@@ -0,0 +1,43 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Hospital.Controllers
+{
+    public class PatientAppointController : Controller
+    {
+        private Mvc_HospitalEntities db = new Mvc_HospitalEntities();
+        // GET: PatientAppoint
+        public ActionResult GetAllDoctors()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var doclist=db.DoctorTables.Where(i=>i.UserTable.IsVerifed==true);  
+            return View(doclist);
+        }
+        public ActionResult GetAllLabs()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var lablist = db.LabTables.Where(i => i.UserTable.IsVerifed == true);
+            return View(lablist);
+        }
+        public ActionResult GetAllPatients()
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var lablist = db.LabTables.Where(i => i.UserTable.IsVerifed == true);
+            return View(lablist);
+        }
+
+    }
+}
