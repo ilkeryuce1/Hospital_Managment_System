1fd48e1c3f26aa6d7409d3723d7eb3539aa33885
ilkeryuce1 <109898238+ilkeryuce1@users.noreply.github.com>
2022-12-31 01:35:21 +0300

    doktor listeleme ve randvu bolumu bır kısmı yapıldı

--- MVC_Hospital/Controllers/PatientAppointController.cs
+++ MVC_Hospital/Controllers/PatientAppointController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 
@@ -37,6 +38,52 @@ namespace MVC_Hospital.Controllers
             }
             var lablist = db.LabTables.Where(i => i.UserTable.IsVerifed == true);
             return View(lablist);
+        }
+
+     
+        public ActionResult DoctorAppointment(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            ViewBag.DoctorTimeSlotID = db.DoctorTimeSlotTables.Where(d => d.DoctorID == id && d.IsActive == true);
+            ViewBag.Doctor = db.DoctorTimeSlotTables.Find(id);
+           
+            return View();
+
+        }
+
+
+
+        public ActionResult DoctorAppointment(DoctorAppointTable appointment)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            appointment.DoctorComment = string.Empty;
+            appointment.IsChecked = false;
+            appointment.IsFeeSubmit = false;
+            if (ModelState.IsValid)
+            {
+                var check = db.DoctorAppointTables.Where(i => i.TransectionID == appointment.TransectionID).FirstOrDefault();
+                var find = db.DoctorAppointTables.Where(p => p.DoctorTimeSlotID == appointment.DoctorTimeSlotID && p.DoctorID == appointment.DoctorID && p.AppointDate == appointment.AppointDate).FirstOrDefault();
+                if (find==null)
+                {
+
+                }
+            }
+
+            return View();
+
+        }
+
+        public ActionResult DoctorProfile(int? id)
+        {
+
+            var doc = db.DoctorTables.Find(id);
+            return View();
         }
 
     }
