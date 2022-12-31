69da53d8ad57ad24cc8a97e408a34d424daf51d2
ilkeryuce1 <109898238+ilkeryuce1@users.noreply.github.com>
2022-12-31 20:19:36 +0300

    Listeleme Özelliklerri Eklendi

--- MVC_Hospital/Controllers/PatientAppointController.cs
+++ MVC_Hospital/Controllers/PatientAppointController.cs
@@ -18,7 +18,7 @@ namespace MVC_Hospital.Controllers
             {
                 return RedirectToAction("Login", "Home");
             }
-            var doclist=db.DoctorTables.Where(i=>i.UserTable.IsVerifed==true);  
+            var doclist = db.DoctorTables.Where(i => i.UserTable.IsVerifed == true);
             return View(doclist);
         }
         public ActionResult GetAllLabs()
@@ -40,22 +40,23 @@ namespace MVC_Hospital.Controllers
             return View(lablist);
         }
 
-     
+
         public ActionResult DoctorAppointment(int? id)
         {
             if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
             {
                 return RedirectToAction("Login", "Home");
             }
-            ViewBag.DoctorTimeSlotID = db.DoctorTimeSlotTables.Where(d => d.DoctorID == id && d.IsActive == true);
+            Session["id"] = id;
+            ViewBag.DoctorTimeSlotID = new SelectList( db.DoctorTimeSlotTables.Where(d => d.DoctorID == id && d.IsActive == true), "DoctorTimeSlotID","Name","0");
             ViewBag.Doctor = db.DoctorTimeSlotTables.Find(id);
-           
+
             return View();
 
         }
 
 
-
+        [HttpPost]
         public ActionResult DoctorAppointment(DoctorAppointTable appointment)
         {
             if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
@@ -65,16 +66,30 @@ namespace MVC_Hospital.Controllers
             appointment.DoctorComment = string.Empty;
             appointment.IsChecked = false;
             appointment.IsFeeSubmit = false;
+            var patient = (PatientTable)Session["Patient"];
+            appointment.PatientID = patient.PatientID;
+            appointment.DoctorID = int.Parse(Convert.ToString(Session["id"]));
             if (ModelState.IsValid)
             {
                 var check = db.DoctorAppointTables.Where(i => i.TransectionID == appointment.TransectionID).FirstOrDefault();
                 var find = db.DoctorAppointTables.Where(p => p.DoctorTimeSlotID == appointment.DoctorTimeSlotID && p.DoctorID == appointment.DoctorID && p.AppointDate == appointment.AppointDate).FirstOrDefault();
-                if (find==null)
+                if (find == null)
                 {
+                    db.DoctorAppointTables.Add(appointment);
+                    db.SaveChanges();
+                    return RedirectToAction("DoctorPendingAppoint");
+                }
+                else
+                {
+                    ViewBag.Message = "Time Slot is Already Assign!(Another Pattient)";
 
                 }
             }
-
+            else
+            {
+                ViewBag.Message = "Transection Number is Already Used! for Another transection!";
+            }
+            ViewBag.DoctorTimeSlotID = new SelectList(db.DoctorTimeSlotTables.Where(d => d.DoctorID == appointment.DoctorID && d.IsActive == true), "DoctorTimeSlotID", "Name", "0");
             return View();
 
         }
@@ -86,5 +101,18 @@ namespace MVC_Hospital.Controllers
             return View();
         }
 
+        public ActionResult DoctorPendingAppoint() 
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["UserName"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            int userid = Convert.ToInt32(Convert.ToString(Session["UserID"]));
+            int pattientid=db.PatientTables.Where(i=>i.UserID== userid).FirstOrDefault().PatientID;
+            var apppointmentlist = db.DoctorAppointTables.Where(i => i.PatientID == pattientid && i.IsFeeSubmit == false && i.IsChecked == false);
+
+            return View(apppointmentlist); 
+        }
+      
     }
 }
